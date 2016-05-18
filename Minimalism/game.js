9c58546072a0932c0fa9d854ab360093731daf32
John Sadie <johnsadie19@gmail.com>
2016-05-18 08:17:27 -0700

    Creating all of my global variables

--- Minimalism/game.js
+++ Minimalism/game.js
@@ -1,27 +1,52 @@
-
-
 var gameport = document.getElementById("gameport");
-
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0xffffff});
+//Refresh rate of browser
+//Size of game
+var renderer = PIXI.autoDetectRenderer(400, 400);
 gameport.appendChild(renderer.view);
 
+/*Creating global variables, stages, and staging*/
+
+var timeKeeper = setInterval(keepTime, 1000);
+var EndGame = false;
 var stage = new PIXI.Container();
+var score = 0;
+var GameOver = new PIXI.Text("Hearts Eaten: " + score, {font:"20px Arial", fill:"black"});
+var scoreBoard = new PIXI.Text("Hearts Eaten: " + score, {font:"20px Arial", fill:"black"});
+scoreBoard.position.x = 0;
+scoreBoard.position.y = 0;
 
-var texture = PIXI.Texture.fromImage("helloworld.jpg");
+var timer = 30;
+var timerBoard = new PIXI.Text("Timer: " + timer, {font:"20px Arial", fill:"black"})
+timerBoard.position.x = 300;
+timerBoard.position.y = 0;
 
-var sprite = new PIXI.Sprite(texture);
+//Beach Sprite
+var beach = new PIXI.Texture.fromImage("beach.png");
+var beachSprite = new PIXI.Sprite(beach);
+beachSprite.anchor.x = 0.5;
+beachSprite.anchor.y = 0.5;
+beachSprite.position.x = 200;
+beachSprite.position.y = 200;
 
-sprite.anchor.x = 0.5;
-sprite.anchor.y = 0.5;
+//Character Sprite
+var zombie = PIXI.Texture.fromImage("zombie.png");
+var zombieSprite = new PIXI.Sprite(zombie);
+zombieSprite.anchor.x = 0.5;
+zombieSprite.anchor.y = 0.5;
+zombieSprite.position.x = 200;
+zombieSprite.position.y = 200;
 
-sprite.position.x = 200;
-sprite.position.y = 200;
+//Heart Sprite
+var heart = PIXI.Texture.fromImage("heart.png");
+var heartSprite = new PIXI.Sprite(heart);
+heartSprite.anchor.x = 0.5;
+heartSprite.anchor.y = 0.5;
+heartSprite.position.x = 100;
+heartSprite.position.y = 100;
 
-stage.addChild(sprite);
-
-function animate() {
-  requestAnimationFrame(animate);
-  sprite.rotation += 0.03;
-  renderer.render(stage);
-}
-animate();
+//Container that holds my elements
+stage.addChild(beachSprite);
+stage.addChild(zombieSprite);
+stage.addChild(heartSprite);
+stage.addChild(scoreBoard);
+stage.addChild(timerBoard);
